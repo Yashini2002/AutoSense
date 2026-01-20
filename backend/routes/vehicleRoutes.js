9d125f5912513cdd3c2c272ec6b8afc8642e8824
Yashini2002 <yashininethma2002@gmail.com>
2026-01-20 10:52:12 +0530

    remove folder

--- backend/routes/vehicleRoutes.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { getVehicles, createVehicle } from "../controllers/vehicleController.js";
-const router = express.Router();
-
-router.get("/", getVehicles);
-router.post("/", createVehicle);
-
-export default router;
